86442dab15f83b89df0ffda2cad651c83eca9b8d
benjaminpstern <bstern@oberlin.edu>
2014-11-20 14:01:52 -0500

    put stuff in

---
+++ Assets/Resources/Scripts/Grass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grass : MonoBehaviour {
+	public float amount;
+	public float regenRate;
+	void Start () {
+		amount = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(amount < 1){
+			amount += regenRate*Time.deltaTime;
+		}
+		else{
+			amount = 1;
+		}
+	}
+}
