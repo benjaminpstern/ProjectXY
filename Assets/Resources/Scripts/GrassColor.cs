2f6da071f1ccfaf9eaff5af603f06ecd11c9951b
benjaminpstern <bstern@oberlin.edu>
2014-11-20 14:33:40 -0500

    did stuff maybe

--- Assets/Resources/Scripts/GrassColor.cs
+++ Assets/Resources/Scripts/GrassColor.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class GrassColor : MonoBehaviour {
 	Grass grass;
-	SpriteRenderer renderer;
+	SpriteRenderer myRenderer;
 	void Start () {
 		grass = this.GetComponent<Grass>();
-		renderer = this.GetComponent<SpriteRenderer>();
+		myRenderer = this.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		renderer.color = new Color((1 - grass.amount),.1f,.1f);
+		myRenderer.color = new Color((1 - grass.amount),.1f,.1f);
 	}
 	void onMouseDown(){
 		grass.amount = 0;
