5b8dcbb6d536a314b9c88f5a83a392d1e62715ce
Cole Peppis <cpeppis@oberlin.edu>
2014-11-20 16:10:20 -0500

    Added a wolf script. Doesn't do much yet, though.

---
+++ Assets/Resources/Scripts/Wolf.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wolf : MonoBehaviour {
+
+	//Things that are initialized differently for each wolf.
+	public int fullness;		//Goes from 1 (Starving) to 10 (Super full).  0 fullness => dead.
+	public float stealth;		//How unlikely it is for the wolf to be noticed. (0 - 1)
+	public float speed;		//How fast the wolf is moving. (0 - 1)
+	public float stamina;		//How much stamina a wolf has; (0 - 1)
+					//How long a wolf can run before it 'poops out.'
+
+	public Vector3 buddies;		//Direction of the center of mass of the visible pack.
+	public Vector3 buddyDir;	//Direction of movement of the visible pack.
+
+	public Buffalo prey;		//Direction of current prey.
+
+	// Use this for initialization
+	void Start () {
+		fullness = Random.Range(3, 7);
+		stealth = Random.Range(0.0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Update buddy vectors
+	void UpdateBuddies () {
+
+	}
+
+	// Look for prey
+	void CheckForPrey () {
+
+	}
+
+	// Move based on prey/buddies
+	void move () {
+
+	}
+}
