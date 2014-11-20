5c9340ec94515f5c87f35dcf93997efb6da8fe84
benjaminpstern <bstern@oberlin.edu>
2014-11-20 14:27:40 -0500

    Stuff

---
+++ Assets/Resources/Scripts/GrassColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassColor : MonoBehaviour {
+	Grass grass;
+	SpriteRenderer renderer;
+	void Start () {
+		grass = this.GetComponent<Grass>();
+		renderer = this.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.color = new Color((1 - grass.amount),.1f,.1f);
+	}
+	void onMouseDown(){
+		grass.amount = 0;
+	}
+}
